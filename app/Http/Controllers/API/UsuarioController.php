e37d9152c2d5615f8d72fc106a9038147dfe8fff
jrami <cljrami@gmail.com>
2024-02-13 14:53:10 -0300

    api con modificaciones

--- app/Http/Controllers/API/UsuarioController.php
+++ app/Http/Controllers/API/UsuarioController.php
@@ -23,6 +23,41 @@ class UsuarioController extends Controller
     public function store(UpdatePasswordRequest $request)
     {
         try{
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // Recibir los datos del formulario HTML
+                $admin_user = $_POST["admin_user"];
+                $admin_pass = $_POST["admin_pass"];
+                $ip = $_POST["ip"];
+                $target_user = $_POST["target_user"];
+                $new_pass = $_POST["new_pass"];
+
+                // Escapar los argumentos del shell
+                $admin_user = escapeshellarg($admin_user);
+                $admin_pass = escapeshellarg($admin_pass);
+                $ip = escapeshellarg($ip);
+                $target_user = escapeshellarg($target_user);
+                $new_pass = escapeshellarg($new_pass);
+
+                // Construir el comando de PowerShell
+                $command = "powershell -Command \"";
+                $command .= "\$securePass = ConvertTo-SecureString -String $admin_pass -AsPlainText -Force; ";
+                $command .= "\$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $admin_user, \$securePass; ";
+                $command .= "Invoke-Command -ComputerName $ip -Credential \$cred -ScriptBlock { ";
+                $command .= "param(\$targetUser, \$newPass); ";
+                $command .= "Set-LocalUser -Name \$targetUser -Password (ConvertTo-SecureString -AsPlainText \$newPass -Force); ";
+                $command .= "} -ArgumentList $target_user, $new_pass; ";
+                $command .= "\"";
+
+                // Ejecutar el comando de PowerShell y obtener la salida
+                $output = shell_exec($command);
+
+                // Mostrar la salida
+                echo "<pre>$output</pre>";
+
+            }
+
+
             if($request->validated()){
                 return response()->json([
                     'res' => true,
@@ -47,9 +82,9 @@ class UsuarioController extends Controller
      */
     public function update(UpdatePasswordRequest $request)
     {
-        
-        
-        
+
+
+
     }
 
     /**
